d2dfb882d115938309a0369fb7e50e4698923aaf
Jonathan Wilferd <jwilferd10@yahoo.com>
2020-11-14 12:42:37 -0800

    Feat: Added new functions that specifically grabs user issues, also redirects user to GitHub issues page

---
+++ assets/js/single.js
@@ -0,0 +1,81 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // Adding text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // Append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+var displayIssues = function(issues) {
+
+    // Checks if repository has no issues
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
+    for (var i = 0; i < issues.length; i++) {
+        // Create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // Create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // Append to container 
+        issueEl.appendChild(titleEl);
+
+        // Create a type element
+        var typeEl = document.createElement("span");
+
+        // Check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        }
+        else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // Append to container
+        issueEl.appendChild(typeEl);
+
+        // Appends issueEl to the HTML
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    
+    fetch(apiUrl).then(function(response) {
+        // Request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                // Pass response data to dom function
+                displayIssues(data);
+
+                // Check if API has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+    console.log(repo);
+};
+  
+getRepoIssues("facebook/react");
