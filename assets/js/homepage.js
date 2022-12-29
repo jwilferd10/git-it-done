c106bc3dde36d9a205304d129ea2ce1635c35db3
Jonathan Wilferd <jwilferd10@yahoo.com>
2022-12-29 01:08:15 -0800

    Feat: Connection to server-side API made and tested with hardcoded JS

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-let getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+let getUserRepos = function(user) {
+    // formatting github api url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make request to url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos("jwilferd10");
